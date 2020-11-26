d1615933a48af040ccb38ebf081e362dda1e53f1
Тимофей Разумов <timofey.razumov@corp.mail.ru>
2020-11-26 20:35:29 +0300

    begin login

---
+++ app/src/main/java/works/drello/LoginViewModel.java
@@ -0,0 +1,98 @@
+package works.drello;
+
+import android.app.Application;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
+
+@SuppressWarnings("WeakerAccess")
+public class LoginViewModel extends AndroidViewModel {
+
+    private LoginData mLastLoginData = new LoginData("", "");
+
+    private final MediatorLiveData<LoginState> mLoginState = new MediatorLiveData<>();
+
+    public LoginViewModel(@NonNull Application application) {
+        super(application);
+        mLoginState.setValue(LoginState.NONE);
+    }
+
+    public LiveData<LoginState> getProgress() {
+        return mLoginState;
+    }
+
+    public void login(String login, String password) {
+        LoginData last = mLastLoginData;
+        LoginData loginData = new LoginData(login, password);
+        mLastLoginData = loginData;
+
+        if (!loginData.isValid()) {
+            mLoginState.postValue(LoginState.ERROR);
+        } else if (last != null && last.equals(loginData)) {
+            Log.w("LoginViewModel", "Ignoring duplicate request with login data");
+        } else if (mLoginState.getValue() != LoginState.IN_PROGRESS) {
+            requestLogin(loginData);
+        }
+    }
+
+
+    private void requestLogin(final LoginData loginData) {
+
+        mLoginState.postValue(LoginState.IN_PROGRESS);
+        final LiveData<AuthRepo.AuthProgress> progressLiveData =
+                AuthRepo.getInstance(getApplication()).login(loginData.getLogin(), loginData.getPassword());
+
+        mLoginState.addSource(progressLiveData, authProgress -> {
+            if (authProgress == AuthRepo.AuthProgress.SUCCESS) {
+                mLoginState.postValue(LoginState.SUCCESS);
+                mLoginState.removeSource(progressLiveData);
+            } else if (authProgress == AuthRepo.AuthProgress.FAILED) {
+                mLoginState.postValue(LoginState.FAILED);
+                mLoginState.removeSource(progressLiveData);
+            }
+        });
+    }
+
+    enum LoginState {
+        NONE,
+        ERROR,
+        IN_PROGRESS,
+        SUCCESS,
+        FAILED
+    }
+
+    public static class LoginData {
+        private final String mLogin;
+        private final String mPassword;
+
+        public LoginData(String login, String password) {
+            mLogin = login;
+            mPassword = password;
+        }
+
+        public String getLogin() {
+            return mLogin;
+        }
+
+        public String getPassword() {
+            return mPassword;
+        }
+
+        public boolean isValid() {
+            return !TextUtils.isEmpty(mLogin) && !TextUtils.isEmpty(mPassword);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            LoginData loginData = (LoginData) o;
+            return mLogin.equals(loginData.mLogin) &&  mLogin.equals(loginData.mPassword);
+        }
+
+    }
+}
--- app/src/main/java/works/drello/MainActivity.java
+++ app/src/main/java/works/drello/MainActivity.java
@@ -9,6 +9,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new LoginFragment(), "Login")
+                    .commit();
+        }
     }
 }
